16832d2dfd88f584880ebe0ed98ff8f0a4ad7cc4
TheFreeman15009 <hritik1509@gmail.com>
2019-09-01 17:15:54 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Racing League India
+                    RLI WEBSITE 
                 </div>
 
                 <div class="links">
