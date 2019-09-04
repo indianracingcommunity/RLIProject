22bb6517ea2494d6e5d273fc897b1854ff578bcd
Kalpesh <kalpeshchoudhary902@gmail.com>
2019-09-04 21:26:29 +0530

    basic layout update

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Racing League Of India</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body class="">
+
+<div class="">
+    <div class="">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">  
+            <ul class="navbar-nav col-11">
+                <li class="nav-item p-3">
+                 <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item p-3">
+                  <a class="nav-link" href="#">Join Us</a>
+                </li>
+                <li class="nav-item p-3">
+                  <a class="nav-link" href="#">Teams And Drivers</a>
+                </li>
+                <li class="nav-item p-3">
+                  <a class="nav-link" href="#">Championship Standings</a>
+                </li>
+                <li class="nav-item p-3">
+                  <a class="nav-link" href="#">About Us</a>
+                </li>
+            </ul>
+            <div class="nav-item col-1">
+              <a class="nav-link" href="#"><strong>Login</strong></a>
+            </div>
+        </nav>
+    </div>
+</div>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,79 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Racing League Of India</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
 
-        <title>Laravel</title>
+<body class="">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <div class="">
+        <div class="">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark">  
+                <ul class="navbar-nav col-11">
+                    <li class="nav-item p-3">
+                       <a class="nav-link" href="#">Home</a>
+                   </li>
+                   <li class="nav-item p-3">
+                      <a class="nav-link" href="#">Join Us</a>
+                  </li>
+                  <li class="nav-item p-3">
+                      <a class="nav-link" href="#">Teams And Drivers</a>
+                  </li>
+                  <li class="nav-item p-3">
+                      <a class="nav-link" href="#">Championship Standings</a>
+                  </li>
+                  <li class="nav-item p-3">
+                      <a class="nav-link" href="#">About Us</a>
+                  </li>
+              </ul>
+              <div class="nav-item col-1">
+                  <a class="nav-link" href="#"><strong>Login</strong></a>
+              </div>
+          </nav>
+      </div>
+  </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <div class=" p-5  text-center">
+        <h1 class="display-2 text-success">Welcome to RLI - "Racing League Of India"</h1>
+    </div>
+<hr>
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Racing League India
-                </div>
-
-                <div class="links">
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Steam</a>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Discord</a>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">News</a>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">GitHub</a>
-                </div>
-            </div>
+<div class="card-columns ">
+    <div class="card" style="width:400px">
+        <div class="card-body">
+            <h2>Card Image</h2>
+            <p>Last Race Results</p>
+            <h4 class="card-title">Belgium GP</h4>
+            <p class="card-text">Race Winner : </p>
+            <a href="#" class="btn btn-primary">Go to Standings</a>
         </div>
-    </body>
+    </div>
+
+    <div class="card" style="width:400px">
+        <div class="card-body">
+            <h2>Card Image</h2>
+            <p>Last Race Results</p>
+            <h4 class="card-title">Belgium GP</h4>
+            <p class="card-text">Race Winner : </p>
+            <a href="#" class="btn btn-primary">Go to Standings</a>
+      </div>
+
+      <div class="card" style="width:400px">
+        <div class="card-body">
+            <h2>Card Image</h2>
+            <p>Last Race Results</p>
+            <h4 class="card-title">Belgium GP</h4>
+            <p class="card-text">Race Winner : </p>
+            <a href="#" class="btn btn-primary">Go to Standings</a>
+      </div>
+</div>
+</div>
+</body>
 </html>
