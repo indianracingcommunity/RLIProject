9a7f263ff517361e9b701f1ecec10413ae7aa724
Kapil Bharath <kapilace6@gmail.com>
2020-05-02 12:09:17 +0530

    Race Results Prep

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,80 @@ Route::get('standings', 'WebsiteController@loadstandings');
 Route::get('aboutus', 'WebsiteController@loadaboutus');
 Route::get('login', 'WebsiteController@loadlogin');
 
-Route::get('image', function()
+Route::get('raceprep', function()
 {
-    $img = Image::make('img/RRSuzuka.png')->resize(1920, 1080);
+    //Name
+    $img = Image::make('img/RRSuzuka.png')
+                ->resize(1920, 1080)
+                ->crop(1000, 90, 90, 215)
+                ->save('img/race_results/Name.png');
+
+    //Standings
+    $img = Image::make('img/RRSuzuka.png')
+                ->resize(1920, 1080)
+                ->crop(1290, 570, 530, 360)
+                ->save('img/race_results/Standings.png');
+
+    $row_width = 40.2142;
+    //Position
+    for($i = 0; $i < 14; $i++) {
+        $pos = Image::make('img/race_results/Standings.png');
+        $pos->crop(50, 33, 3, 7 + (int)($i * $row_width));
+        $pos->save('img/race_results/pos_' . ($i + 1) . '.png');
+    }
+
+    //Driver
+    for($i = 0; $i < 14; $i++) {
+        $driver = Image::make('img/race_results/Standings.png');
+        $driver->crop(150, 33, 150, 7 + (int)($i * $row_width));
+        $driver->save('img/race_results/driver_' . ($i + 1) . '.png');
+    }
+
+    //Team
+    for($i = 0; $i < 14; $i++) {
+        $team = Image::make('img/race_results/Standings.png');
+        $team->crop(240, 33, 555, 7 + (int)($i * $row_width));
+        $team->save('img/race_results/team_' . ($i + 1) . '.png');
+    }
+
+    //Grid
+    for($i = 0; $i < 14; $i++) {
+        $team = Image::make('img/race_results/Standings.png');
+        $team->crop(50, 33, 821, 7 + (int)($i * $row_width));
+        $team->save('img/race_results/grid_' . ($i + 1) . '.png');
+    }
+
+    //Stops
+    for($i = 0; $i < 14; $i++) {
+        $team = Image::make('img/race_results/Standings.png');
+        $team->crop(50, 33, 910, 7 + (int)($i * $row_width));
+        $team->save('img/race_results/stops_' . ($i + 1) . '.png');
+    }
+
+    //Fastest Lap
+    for($i = 0; $i < 14; $i++) {
+        $team = Image::make('img/race_results/Standings.png');
+        $team->crop(150, 33, 1000, 7 + (int)($i * $row_width));
+        $team->save('img/race_results/best' . ($i + 1) . '.png');
+    }
+
+    //Finishing Time
+    for($i = 0; $i < 14; $i++) {
+        $team = Image::make('img/race_results/Standings.png');
+        $team->crop(240, 33, 1175, 7 + (int)($i * $row_width));
+        $team->save('img/race_results/time_' . ($i + 1) . '.png');
+    }
+
+    /*  Race Name
+    ->crop(1000, 90, 90, 215);
+    $img->save('img/RRSuzukaName.png');
+    */
+
+    /*  Race Standings
+    ->crop(1290, 570, 530, 360);
+    $img->save('img/RRSuzukaResults.png');
+    */
+
 
     return $img->response('png');
 });
