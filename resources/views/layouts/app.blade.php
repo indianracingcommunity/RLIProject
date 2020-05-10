74cfb0da3c614c628758537b38cd5af79089c0b1
Snehasish Dawn <snehasishdawn29@gmail.com>
2020-05-10 14:14:16 +0530

    standings page ui update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,17 +10,18 @@
     <title>RLI Admin Panel</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head>
 <body>
-    <div id="app">
+    <!-- <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
@@ -30,16 +31,16 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <!-- <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                    </ul> -->
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <!-- <ul class="navbar-nav ml-auto"> -->
                         <!-- Authentication Links -->
-                        @guest
+                        <!-- @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -48,8 +49,8 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('') }}</a>
                                 </li>
                             @endif
-                        @else
-                            <li class="nav-item dropdown">
+                        @else -->
+                            <!-- <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -76,7 +77,38 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> -->
+    
+        <nav class="flex justify-between border-b">
+         <div class="flex py-2">
+               <div class="px-3 bg-gray-800 mx-2 text-white font-bold rounded-md hover:bg-gray-700 cursor-pointer">
+                  <a href="/"   class="flex" class="px-3 bg-gray-800 mx-2 text-white font-bold rounded-md hover:bg-gray-700" ><img src="/img/IRC_logo/logo_square.png"height="45" width="45"> <span class="p-3">Indian Racing Comunity</span> </a>
+               </div>
+         </div>
+         @auth
+         <div class="flex">
+            <div >
+            <div class="px-4 flex py-2 m-2 bg-gray-100 rounded font-semibold border cursor-pointer hover:bg-gray-200 hover:shadow-none">
+                <a class="dropdown-item" href="/user/profile/{{Auth::user()->id}}">
+                    {{Auth::user()->name}}
+                </a>
+            </div>
+            </div>
+            <div>
+                <div class="px-4 flex py-2 m-2 bg-red-600 text-white rounded font-semibold cursor-pointer hover:bg-red-700 hover:shadow-none">
+                <a  href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                        Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    {{ csrf_field() }}
+                </form>
+            </div>
+         </div>
+         @endauth
+      </nav>
 
         <main class="py-4">
            
--- resources/views/standings/season.blade.php
+++ resources/views/standings/season.blade.php
@@ -1,30 +1,176 @@
 @extends('layouts.app')
+<style>
+tr:nth-child(even) td {
+  background-color: #EDF2F7;
+  margin:5px;
+}
+table {
+  width: 100%
+}
+th {
+  padding: 10px;
+  text-align: left;
+}
+tr:hover, td:hover {background-color: #E2E8F0;}
+td {
+  padding-left:10px;
+  padding-top:5px;
+  padding-bottom: 5px;
+}
+.card1 {
+  background:linear-gradient(45deg,#f09819,#edde5d);
+
+}
+
+.card2 {
+  /* background:linear-gradient(45.34deg, #EA52F8 5.66%, #0066FF 94.35%); */
+  background-color: #2f4353;
+background-image: linear-gradient(315deg, #d2ccc4 0%,#2f4353 74%);
+
+}
+.card3 {
+  /* background:linear-gradient(45deg, #191654, #43C6AC);
+   */
+  background: #B79891;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #94716B, #B79891);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #94716B, #B79891); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+
+}
+</style>
+
 @section('content')
-<div class="container">
-    <h2>IRC Standings</h2>
-    <p>Standings for Season : </p>            
+<div class="container mx-auto flex">
+  <div class="w-1/4">
+  <div class="text-4xl font-bold text-gray-800">
+    <i class="fas fa-chess-king text-purple-600"></i> Tier 1
+  </div>
+  <div class="text-2xl font-semibold text-gray-700">
+    Season 4
+  </div>
+  <div class="bg-gray-100 rounded-md px-4 py-2 my-4">
+    <div class="text-sm font-semibold my-2">
+      Constructor Standings
+    </div>
+    <table>
+      <thead>
+      <tr>
+        <th class="rounded-md bg-gray-300 border-2 border-white">Teams</th>
+        <th  class="rounded-md bg-gray-300 border-2 border-white">Points</th>
+      </tr>
+      </thead>
+      <tbody>
+      @for ($i = 0 ; $i < 10; $i++)
+        <tr class="cursor-pointer">
+          <td class="font-semibold rounded-lg border border-white">
+            Haass
+          </td>
+          <td class="font-semibold rounded-lg border border-white">
+            123
+          </td>
+        </tr>
+      @endfor 
+      </tbody>
+    </table>
+  
+  </div>
+  <div class="border rounded-md p-3">
+    <div class="text-xl font-semibold text-gray-600">
+      Next Race
+    </div>
+    <div class="text-4xl font-semibold text-purple-700">
+      Great Britain
+    </div>
+    <div class="mb-4">
+      <img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Great_Britain_Circuit.png.transform/9col/image.png" alt="">
+    </div>
+    <div class="flex justify-between font-semibold">
+      <div>
+        Circuit Length
+      </div>
+      <div class="text-lg text-blue-700">
+        5.891 km
+      </div>
+    </div>
+    <div class="flex justify-between font-semibold">
+      <div>
+        Number of laps
+      </div>
+      <div class="text-lg text-blue-700">
+       30
+      </div>
+    </div>
+  </div>
+  </div>
+  <div class="w-3/4 mx-5">
+    <div class="flex mb-6">
+      <div class="card1 mr-8 text-white p-4 rounded-md hover:shadow-lg w-1/6">
+        <div class="text-4xl font-bold">
+          1st
+        </div>
+        <div class="text-sm font-semibold">
+          Ferrari
+        </div>
+        <div class="font-semibold">
+          MaranelloBaby
+        </div>
+      </div>
+
+      <div class="card2 mr-8 text-white p-4 rounded-md hover:shadow-lg w-1/6">
+        <div class="text-4xl font-bold">
+          2nd
+        </div>
+        <div class="text-sm font-semibold">
+          Haas
+        </div>
+        <div class="font-semibold">
+          GeneralPepe
+        </div>
+      </div>
+
+      <div class="card3 mr-8 text-white p-4 rounded-md hover:shadow-lg w-1/6">
+        <div class="text-4xl font-bold">
+          3rd
+        </div>
+        <div class="text-sm font-semibold">
+          Ferrari
+        </div>
+        <div class="font-semibold">
+          dawn29
+        </div>
+      </div>
+    </div>
+
+
+    <div class="text-sm font-semibold">
+    Drivers' Standings
+    </div>
     <table class="table">
       <thead>
         <tr>
-          <th>Firstname</th>
-          <th>Points</th>
+          <th class="rounded-md bg-gray-300 border-2 border-white">Position</th>
+          <th class="rounded-md bg-gray-300 border-2 border-white">Driver</th>
+          <th class="rounded-md bg-gray-300 border-2 border-white">Team</th>
+          <th class="rounded-md bg-gray-300 border-2 border-white">Points</th>
         </tr>
-    </thead>
-@for ($i = 0 ; $i < $count; $i++)
-<tbody>
-    <tr>
-      <td>{{$res[$i]['name']}}</td>
-      <td>{{$res[$i]['points']}}</td>
-    </tr> 
-
-
-
-    
-
-@endfor
-</tbody>
-</table>
-</div>
+      </thead>
+      <tbody>
+        @for ($i = 0 ; $i < $count; $i++)
+          <tr class="cursor-pointer">
+            <td class="font-semibold rounded-lg border border-white">{{$i+1}}</td>
+            <td class="font-semibold rounded-lg border border-white">{{$res[$i]['name']}}</td>
+            <td class="font-semibold rounded-lg border border-white">
+              <span class="border-l-4 px-2 border-red-600">
+                Ferrari
+              </span>
+            </td>
+            <td class="font-semibold rounded-lg border border-white">{{$res[$i]['points']}}</td>
+          </tr> 
+        @endfor
+      </tbody>
+    </table>
+  </div>
   
 @endsection
 
