85d818dd04cf713ea3cae384a9ea10857ae3abf7
Snehasish Dawn <snehasishdawn29@gmail.com>
2020-05-10 15:28:52 +0530

    updated race pages

--- resources/views/standings/allraces.blade.php
+++ resources/views/standings/allraces.blade.php
@@ -13,26 +13,42 @@ td {
 }
 </style>
 @section('content')
-<div class="container mx-auto w-2/3 flex">
-  <div class="1/3">
-    Tier 1
+<div class="container mx-auto w-2/3">
+<div class="flex">
+  <div class="w-1/4">
+    <div class="text-4xl font-bold text-gray-800">
+      <i class="fas fa-chess-king text-purple-600"></i> Tier 1
+    </div>
+    <div class="text-2xl font-semibold text-gray-700">
+      Season 4
+    </div>
+    <div class="bg-indigo-100 font-semibold p-3 rounded-md my-2">
+      This is the All races page here you can select any race and get details about that particular race
+    </div>
   </div>
-  <div class="2/3">
+  <div class="w-3/4 mx-4">
       <h2 class="text-3xl font-semibold">All Races </h2>           
-      <table class="table">
+      <table>
         <thead>
           <tr>
-            <th class="bg-gray-300 rounded-md">Tracks</th>
+            <th class="bg-gray-300 rounded-md w-24 text-center border-2 border-white">Round</th>
+            <th class="bg-gray-300 rounded-md border-2 border-white">Tracks</th>
           </tr>
       </thead>
       <tbody>
-  @foreach($races as $value)
+  @foreach($races as $ie=>$value)
       <tr>
+      <td class="rounded-md border-2 border-white font-semibold">
+        <div class="py-2 text-center">
+        {{$ie+1}}
+        </div>
+      </td>
       <td class="rounded-md border-2 border-white font-semibold flex justify-between">
       <div class="py-2">
         {{$value->circuit->name}}
-      </div>   
-      <a href="/{{$value->season->tier}}/{{$value->season->season}}/race/{{$value->id}}" class="float-right bg-blue-600 rounded text-white font-semibold p-2 hover:bg-blue-700">View Results</a>
+      </div> 
+      </tf>  
+      <a href="/{{$value->season->tier}}/{{$value->season->season}}/race/{{$value->id}}" class="float-right bg-gray-100 rounded text-gray-800 font-semibold p-2 hover:bg-indigo-100 hover:text-indigo-800">View Results</a>
       </td>
       </tr> 
   @endforeach
@@ -40,6 +56,7 @@ td {
   </table>
 
   </div>
+</div>
 </div>
   
 @endsection
--- resources/views/standings/race.blade.php
+++ resources/views/standings/race.blade.php
@@ -1,28 +1,103 @@
 @extends('layouts.app')
+<style>
+tr:nth-child(even) td {
+  /* background-color: #EDF2F7; */
+}
+.fast {
+  backgroun-color: red !important;
+}
+table {
+  width: 100%
+}
+th {
+  padding: 10px;
+  text-align: left;
+}
+tr:hover, td:hover {background-color: #E2E8F0;}
+td {
+  padding-left:10px;
+  padding-top:5px;
+  padding-bottom: 5px;
+}
+</style>
 @section('content')
-<div class="container">
-    <h2>{{$results[0]['race']['circuit']['name']}} </h2>           
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Driver</th>
-          <th>Points</th>
-        </tr>
-    </thead>
-@for ($i = 0 ; $i < $count; $i++)
-<tbody>
+<div class="container w-2/3 mx-auto">
+  <div class="flex">
+    <div class="w-1/4"> 
+    <div class="border rounded-md p-3">
+    <div class="text-4xl font-semibold text-purple-700">
+    {{$results[0]['race']['circuit']['name']}}
+    </div>
+    <div class="mb-4">
+      <img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Australia_Circuit.png.transform/9col/image.png" alt="">
+    </div>
+    <div class="flex justify-between font-semibold">
+      <div>
+        Circuit Length
+      </div>
+      <div class="text-lg text-blue-700">
+        5.303 km
+      </div>
+    </div>
+    <div class="flex justify-between font-semibold">
+      <div>
+        Number of laps
+      </div>
+      <div class="text-lg text-blue-700">
+       30
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="w-3/4 mx-4">
+  <div class="font-semibold">
+    Drivers` Standing
+  </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th class="rounded-md bg-gray-300 border-2 border-white w-8">Position</th>
+            <th class="rounded-md bg-gray-300 border-2 border-white">Driver</th>
+            <th class="rounded-md bg-gray-300 border-2 border-white">Points</th>
+          </tr>
+      </thead>
+  <tbody>
+  @for ($i = 0 ; $i < $count; $i++)
+  @if($results[$i]['fastestlap'] == 1)
+      <tr>
+        <td class="font-bold rounded-lg border border-white bg-purple-200 text-purple-700 fast">{{$i+1}}</td>
+        
+        <td class="font-bold rounded-lg border border-white bg-purple-200 text-purple-700">{{$results[$i]['driver']['name']}}</td>
+        
+        <td class="font-bold rounded-lg border border-white bg-purple-200 text-purple-700">{{$results[$i]['points']}}</td>
+      </tr> 
+    @elseif($i%2 != 0)
     <tr>
-      <td>{{$results[$i]['driver']['name']}}</td>
-      <td>{{$results[$i]['points']}}</td>
-    </tr> 
+        <td class="font-semibold rounded-lg border border-white bg-gray-200">{{$i+1}}</td>
+        
+        <td class="font-semibold rounded-lg border border-white bg-gray-200">{{$results[$i]['driver']['name']}}</td>
+        
+        <td class="font-semibold rounded-lg border border-white bg-gray-200">{{$results[$i]['points']}}</td>
+      </tr> 
+    @else
+      <tr>
+        <td class="font-semibold rounded-lg border border-white">{{$i+1}}</td>
+        
+        <td class="font-semibold rounded-lg border border-white">{{$results[$i]['driver']['name']}}</td>
+        
+        <td class="font-semibold rounded-lg border border-white">{{$results[$i]['points']}}</td>
+      </tr> 
+    @endif
 
 
 
-    
+      
 
-@endfor
-</tbody>
-</table>
+  @endfor
+  </tbody>
+  </table>
+  
+  </div>          
 </div>
   
 @endsection
