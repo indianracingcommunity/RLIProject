a95f3d53a7838383d819ad7f5a3e959ddbdf78a2
TheFreeman15009 <hritik1509@gmail.com>
2020-06-19 12:02:11 +0530

    Updated Steam Alias check to work with Oauth login

--- app/Http/Controllers/SteamController.php
+++ app/Http/Controllers/SteamController.php
@@ -20,48 +20,22 @@ class SteamController extends Controller
 
         for($i = 0; $i < $count; $i++)
         {
-            // Parsing the URL to trim it
-            $url = parse_url($query[$i]['user']['steam_id']);
-
-            if(strpos($url['path'],'profile'))  // Checking Which type of steam link is provider
-            {
-                $trim = trim($url['path'], '/profiles');  // Trimming the Url string
-        
-                // Calling the Steam API to fetch the latest username
-                $str = $this->querySteam('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$key.'&steamids='.$trim);
-                $json = json_decode($str, true);
-
-                if($json['response']['players'] == NULL)
+            
+                $str = $this->querySteam('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$key.'&steamids='.$query[$i]['user']['steam_id']);
+                //dd($str['response']);
+                
+                if($str['response']['players'] == NULL)
                     echo "Invalid Steam ID";
                 else
                 {
-                    $checkalias = $json['response']['players']['0']['personaname'];
+                    $checkalias = $str['response']['players']['0']['personaname'];
                     echo "<br>".$checkalias." Driver id ".$query[$i]['id']."<br>";
                     $query[$i]->insertAlias($checkalias);
                 }
             }
 
-            // Handling Vanity Urls
-            elseif (strpos($url['path'], 'id'))
-            {
-                // Resolving Vanity URL to Steamid64
-                $trim = trim($url['path'], '/id');
-                $json = $this->querySteam('https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key='.$key.'&vanityurl='.$trim);
-                if($json['response']['success'] == 1)
-                {
-                    $steam64 = $json['response']['steamid'];
-                    $json = $this->querySteam('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$key.'&steamids='.$steam64);
-
-                    $checkalias = $json['response']['players']['0']['personaname'];
-                    echo "<br>" . $checkalias . " Driver id ". $query[$i]['id'] . "<br>";
-                    $query[$i]->insertAlias($checkalias);
-                }
-                else
-                    echo "Invalid Vanity URL";
-            }
-            else
-                echo "Invalid URL";
-        }
+            // Handling Vanity Urls     
+        
     }
 
     protected function querySteam(String $query)
