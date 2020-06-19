a4e72ab958d928f2d87bb21d3a36877b9dc32a8e
Kapil Bharath <kapilace6@gmail.com>
2020-06-19 15:58:23 +0530

    Steam Profile Sign In Clean Up

--- resources/views/user/userprofile.blade.php
+++ resources/views/user/userprofile.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-<div class="font-semibold text-orange-800 bg-orange-200 rounded-md p-8">
-    Welcome to <strong>Indian Racing Comunity!</strong> Your account is created but not yet <strong>verified</strong>. To verify your Account please add your <strong>Steam ID</strong> on the given field
-</div>
+@if (Auth::user()->steam_id == NULL)
+ <div class="font-semibold text-orange-800 bg-orange-200 rounded-md p-8">
+     Welcome to <strong>Indian Racing Comunity!</strong> Your account is created but not yet <strong>verified</strong>. To verify your Account please add your <strong>Steam ID</strong> on the given field
+ </div>
+@endif
+
 <div class="flex my-8">
     <div>
         <img src="{{$user->avatar}}" class="rounded-md " alt="">
@@ -28,20 +31,17 @@
     </div>
 </div>
 <div>
-<form method="POST" action="setsteam/{{$user->id}}">
-    @csrf
-    
-    <br><br>
-    @if (Auth::user()->steam_id != NULL)
-        
-    
-    @else
-    <span class="text-xs font-semibold text-gray-600 mt-1">STEAM PROFILE LINK</span> <span class="text-red-600 mr-4">●</span>
-   <a href="/login/steam"> <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png" alt=""> </a>
-    </div>
-        <span class="text-red-600 mr-2">●</span><span class="text-xs font-semibold text-gray-700">To verify your account add your Steam profile link</span>
-    @endif
-    <div>
-</form>
+    <form method="POST" action="setsteam/{{$user->id}}">
+        @csrf
+        <br><br>
+
+        @if (Auth::user()->steam_id == NULL)
+         <span class="text-xs font-semibold text-gray-600 mt-1">STEAM PROFILE LINK</span>
+         <span class="text-red-600 mr-4">●</span>
+         <a href="/login/steam"> <img src="{{url('/storage/img/steam.png')}}" alt=""> </a>
+         <span class="text-red-600 mr-2">●</span><span class="text-xs font-semibold text-gray-700">To verify your account add your Steam profile link</span>
+        @endif
+    </form>
+</div>
 
 @endsection
