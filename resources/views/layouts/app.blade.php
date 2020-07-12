9abbd2aa5f1840edf8fa769c7a1dba53861b89a1
Kalpesh <kalpesh.c@happynessfactory.in>
2020-07-12 22:37:58 +0530

    dropdown fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,11 +28,11 @@
                         <button class="font-semibold px-4 rounded inline-flex items-center">
                             <span> <i class='fas fa-trophy mx-1 text-yellow-500'></i> Championship Standings</span>
                         </button>
-                        <ul class="dropdown-content absolute hidden text-gray-700 pt-1 pl-2">
+                        <ul class="dropdown-content absolute hidden text-gray-700 w-56 pt-2">
                             @foreach($topBarSeasons as $series) 
                                 <li class="dropdown">
                                     <a class="bg-indigo-100 hover:bg-blue-300 py-2 px-4 block whitespace-no-wrap rounded" href="#"><i class='fas fa-caret-right pr-3 text-green-500'></i> {{$series['name']['name']}}</a>
-                                    <ul class="dropdown-content absolute hidden text-gray-700 pl-12 ml-24 -mt-10">
+                                    <ul class="dropdown-content hidden absolute text-gray-700 pl-8 ml-48 -mt-10">
                                         @foreach($series['tier'] as $tier)
                                         <li class="dropdown">
                                             <a class="bg-orange-100 hover:bg-green-300 py-2 px-4 block whitespace-no-wrap rounded" href="/{{$series['name']['code']}}/{{$tier[0]['tier']}}/{{$tier[0]['season']}}/standings"><i class='fas fa-caret-right pr-3 text-blue-500'></i> Tier {{$tier[0]['tier']}}</a>
@@ -54,11 +54,11 @@
                         <button class="font-semibold px-4 rounded inline-flex items-center">
                             <span>🏁 Race Results</span>
                         </button>
-                        <ul class="dropdown-content absolute hidden text-gray-700 pt-1 pl-2">
+                        <ul class="dropdown-content absolute hidden text-gray-700 w-56 pt-2">
                             @foreach($topBarSeasons as $series) 
                                 <li class="dropdown">
                                     <a class="bg-indigo-100 hover:bg-blue-300 py-2 px-4 block whitespace-no-wrap rounded" href="#"><i class='fas fa-caret-right pr-3 text-green-500'></i> {{$series['name']['name']}}</a>
-                                    <ul class="dropdown-content absolute hidden text-gray-700 pl-12 ml-24 -mt-10">
+                                    <ul class="dropdown-content hidden absolute text-gray-700 pl-8 ml-48 -mt-10">
                                         @foreach($series['tier'] as $tier)
                                         <li class="dropdown">
                                             <a class="bg-orange-100 hover:bg-green-300 py-2 px-4 block whitespace-no-wrap rounded" href="/{{$series['name']['code']}}/{{$tier[0]['tier']}}/{{$tier[0]['season']}}/races"><i class='fas fa-caret-right pr-3 text-blue-500'></i> Tier {{$tier[0]['tier']}}</a>
