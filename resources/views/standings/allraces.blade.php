3d3c16cdf66bed0a5238cd43da5767b704eaa307
Dipanshuprasad <prasad.dipanshu.prasad@gmail.com>
2020-07-16 15:08:27 +0530

    Update allraces.blade.php

--- resources/views/standings/allraces.blade.php
+++ resources/views/standings/allraces.blade.php
@@ -13,10 +13,9 @@ padding-left:10px;
 }
 </style>
 @section('content')
-
-   <div class="block sm:block md:block lg:flex xl:flex justify-center w-full">
-      
-      <div class="block pt-5 justify-center items-center w-full sm:w-full md:w-full lg:w-1/6 xl:w-1/6 px-1 sm:px-1 md:px-1 lg:px-4 xl:px-4">
+<div class="container mx-auto w-11/12">
+   <div class="flex">
+      <div class="w-1/4">
          @if($season['season'] == (int)$season['season'])
          <div class="text-4xl font-bold text-gray-800 leading-tight">
             <i class="fas fa-chess-king text-purple-600"></i> Tier {{$season['tier']}}
@@ -33,13 +32,13 @@ padding-left:10px;
             Welcome to the Results Page of this Season.
          </div>
       </div>
-      <div class="w-full sm:w-full md:w-full lg:w-2/5 xl:w-2/5 pt-5 px-1 sm:px-1 md:px-1 lg:px-10 xl:px-10">
-         <h2 class="text-3xl font-semibold">All Races </h2>
-         <table class="w-full">
+      <div class="w-5/6 mx-4">
+         <h2 class="text-3xl font-semibold ml-20">All Races </h2>
+         <table class="w-5/6 mx-auto">
             <thead>
                <tr>
-                  <th class="bg-gray-300 border rounded-md text-center border-2 border-white">Round</th>
-                  <th class="bg-gray-300 border rounded-md border-2 border-white">Tracks</th>
+                  <th class="bg-gray-300 rounded-md w-24 text-center border-2 border-white">Round</th>
+                  <th class="bg-gray-300 rounded-md border-2 border-white">Tracks</th>
                </tr>
             </thead>
             <tbody>
@@ -50,14 +49,16 @@ padding-left:10px;
                         {{$value->round}}
                      </div>
                   </td>
-                  <td class="rounded-md border-2 border-white font-semibold flex justify-between items-center">
-                     <div class="py-2 w-2/6 truncate flex items-center flex-shrink-0">
+                  <td class="rounded-md border-2 border-white font-semibold flex justify-between">
+                     <div class="py-2 flex items-center flex-shrink-0">
+                        <div class="flex items-center flex-shrink-0">
                            {{$value->circuit->name}}
+                        </div class="flex items-center flex-shrink-0">
+
                      </div>
-                     <div class="flex items-center">
-                        <img src="{{$value->circuit->flag}}" class="w-12 mr-2 sm:mx-2 md:mx-2 lg:mr-8 xl:mr-8 border rounded float-right" alt="">
+                     <div>
                         <a href="/{{$code}}/{{$value->season->tier}}/{{$value->season->season}}/race/{{$value->round}}" class="float-right bg-gray-100 rounded text-gray-800 font-semibold p-2 hover:bg-indigo-100 hover:text-indigo-800">View Results</a>
-                        
+                        <img src="{{$value->circuit->flag}}" class="w-12 mr-8 border rounded float-right" alt="">
                      </div>
                   </td>
                </tr>
@@ -66,5 +67,5 @@ padding-left:10px;
          </table>
       </div>
    </div>
-
+</div>
 @endsection
