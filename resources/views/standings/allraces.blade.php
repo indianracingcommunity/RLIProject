f728bd14dccf7523a55d2a9679f5c9b79a27cdba
Kalpesh <kalpeshchoudhary902@gmail.com>
2020-07-23 20:45:58 +0530

    fuk kapil

--- resources/views/standings/allraces.blade.php
+++ resources/views/standings/allraces.blade.php
@@ -7,15 +7,17 @@ th {
 padding: 10px;
 text-align: left;
 }
-
 td {
 padding-left:10px;
 }
 </style>
+@php 
+Log::info(print_r($tdrivers,true));
+@endphp
 @section('content')
 <div class="container mx-auto w-11/12">
    <div class="flex">
-      <div class="w-1/4">
+      <div class="w-2/6">
          @if($season['season'] == (int)$season['season'])
          <div class="text-4xl font-bold text-gray-800 leading-tight">
             <i class="fas fa-chess-king text-purple-600"></i> Tier {{$season['tier']}}
@@ -28,12 +30,75 @@ padding-left:10px;
             <i class="fas fa-chess-king text-purple-600"></i> {{$season['name']}}
          </div>
          @endif
-         <div class="bg-indigo-100 font-semibold p-3 rounded-md my-2">
+         {{-- <div class="bg-indigo-100 font-semibold p-3 rounded-md my-2">
             Welcome to the Results Page of this Season.
+         </div> --}}
+         <div class="bg-purple-100 mt-2">
+            <div class="text-2xl text-purple-700  rounded-md bg-purple-200 text-center p-4 leading-none cf font-bold">
+               Top 3 Drivers
+            </div>
+            @for ($i = 0, $k = 0; $i < count($tdrivers) && $k < 3; $i++, $k++)
+            @php
+            if((abs($tdrivers[$i]['status']) >= 10 && abs($tdrivers[$i]['status']) < 20) || $tdrivers[$i]['team']['name'] == 'Reserve')
+            {
+            $k--;
+            continue;
+            }
+            @endphp
+            @if($k == 0)
+            <div class="border hover:shadow-lg shadow-md text-center bg-yellow-300 rounded-md leading-none">
+               <div class="text-2xl font-bold p-4">
+                  1st : {{$tdrivers[$i]['name']}}
+               </div>
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+            @endif
+            @if($k == 1)
+            <div class="border hover:shadow-lg shadow-md mt-2 text-center bg-gray-200 rounded-md leading-none">
+               <div class="text-2xl font-bold p-4">
+                  2nd : {{$tdrivers[$i]['name']}}
+               </div>
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+            @endif
+            @if($k == 2)
+            <div class="border hover:shadow-lg shadow-md mt-2 text-center bg-orange-200 rounded-md leading-none">
+               <div class="text-2xl font-bold p-4">
+                  3rd : {{$tdrivers[$i]['name']}}
+               </div>
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+            @endif
+            @endfor
          </div>
-         
+         <div class="bg-purple-100 ">
+            <div class="text-2xl text-purple-700 text-center rounded-md bg-purple-200 text-center p-4 mt-3 leading-none cf font-bold">
+               Top 3 Constructors
+            </div>
+            <div class="border hover:shadow-lg shadow-md  text-center bg-yellow-300 rounded-md leading-none">
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+            <div class="border hover:shadow-lg shadow-md mt-2 text-center bg-gray-200 rounded-md leading-none">
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+            <div class="border hover:shadow-lg shadow-md mt-2 text-center bg-orange-200 rounded-md leading-none">
+               <div class="flex justify-center p-2">
+                  <img class="h-10" src="https://www.f1gamesetup.com/img/teams/2019/mclaren-2019.png">
+               </div>
+            </div>
+         </div>
       </div>
-      <div class="w-5/6 mx-4">
+      <div class="w-5/6">
          <h2 class="text-3xl font-semibold ml-20">All Races </h2>
          <table class="w-5/6 mx-auto">
             <thead>
