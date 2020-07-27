076415ed80937d6a3738bf7fa2e46f9d611ef043
Kapil Bharath <kapilace6@gmail.com>
2020-07-27 15:32:37 +0530

    Time Conversion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,37 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    static public function convertMillisToStandard($time)
+    {
+        $mtime = (int)$time;
+        $seconds = $mtime / 1000.0;
+        $minutes = (int)$seconds % 60;
+        $seconds = $seconds - $minutes * 60;
+
+        $res = "";
+        if($minutes > 0)
+            $res += (string)$minutes + ":";
+        if($seconds < 10)
+            $res += "0";
+
+        $res += (string)$seconds;
+        return $res;
+    }
+
+    static public function convertStandardtoMillis($time)
+    {
+        $seg_time = explode(":", $time);
+        $min = 0;
+        $sec = 0;
+        if(count($seg_time) > 0)
+            $min = (int)$seg_time[0];
+        if(count($seg_time) > 0)
+            $sec = (float)$seg_time[1];
+        else
+            $sec = (float)$seg_time[0];
+
+        $res = $min * 60 + $sec;
+        return $res;
+    }
 }
