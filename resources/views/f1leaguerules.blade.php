e1d3dd5656e9fee617e0d885fb11657f81f58989
Kalpesh <kalpeshchoudhary902@gmail.com>
2020-08-01 17:33:32 +0530

    oopsie

---
+++ resources/views/f1leaguerules.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.welcomeLayout')
+
+@section('body')
+<div class="container mx-auto w-full">
+	<iframe width="100%" style="height: -webkit-fill-available;" src="https://docs.google.com/document/d/1Kn8Pmd_AG42X4MgARYpX_jtNb0OnRxyC0iPfAVRLjBo/edit?usp=sharing" title="F1 League Rules"></iframe>
+</div>
+@endsection
