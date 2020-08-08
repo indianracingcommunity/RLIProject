6692da597a73ea9fe9305120adec70c7feee72e5
TheFreeman15009 <hritik1509@gmail.com>
2020-08-08 11:19:58 +0530

    Changes to driver allot page

--- resources/views/admin/allot.blade.php
+++ resources/views/admin/allot.blade.php
@@ -19,15 +19,30 @@
                 <form method="POST" action="/home/admin/user-allot/submit">
                     @csrf
                 <input type="hidden" value="{{$user->id}}" name="user_id">
-                    Team: <select name="constructor" id="constructor">
-                        @foreach($team as $value)
-                    <option value="{{$value->id}}">{{$value->name}}</option>
-                        @endforeach
+                    Team: <select name="tier" id="tier">
+                       
+                    <option value="1">Tier 1</option>
+                    <option value="2">Tier 2</option>
+                    <option value="3">Reserve/Challenger</option>
                     </select>
                     
                 <br>
                 <br>
+                @if (count($exisiting)==0)       
                 <input type="submit" class="btn btn-warning">
+                @else
+                
+                @endif
+
+                @if (count($exisiting) > 1 )
+              <div class="bg-red-200 rounded text-red-800 p-4 mb-3 font-semibold">
+                 Duplicate Drivers in DB detected please check the database before proceeding !   
+                </div>
+                    
+                @endif
+                 
+                
+                
                 </div>
               
             </div>
