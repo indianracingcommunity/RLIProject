8e06a4bfdd4364054fee2cb49306c5d1ea6a1b7b
TheFreeman15009 <hritik1509@gmail.com>
2020-08-23 13:24:25 +0530

    Counter for users page

--- resources/views/admin/viewusers.blade.php
+++ resources/views/admin/viewusers.blade.php
@@ -9,8 +9,16 @@
 <div class="block text-gray-700 text-2xl font-bold p-10">All Users</div>
 <div class="flex w-1/2 sm:w-full md:w-full lg:w-full xl:w-full justify-center bg-white shadow-lg rounded px-4 pb-4" style="display: flex;">
 <table class="table-auto rounded-lg shadow-lg">
+  @php
+  $i = 1     
+  @endphp
+
+  
   @foreach($user as $user)
   <tr>
+    <td>
+      {{$i}}
+    </td>
     <td>
       <img src="{{$user->avatar}}" alt="" style="width:30px" class="rounded">
     </td>
@@ -24,6 +32,9 @@
       <a href="/home/admin/user-allot/{{$user->id}}" class="bg-yellow-100 rounded py-2 px-4 text-yellow-800 cursor-pointer hover:text-yellow-900 hover:bg-yellow-200 ">Allot Driver</a> 
     </td>
   </tr>
+  @php
+  $i++     
+  @endphp
   @endforeach
           
 </table>
