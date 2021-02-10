3c14cbd5c761a53ef1d0f1ce4bd2152a91419f6c
theKC66 <53808244+thekalpeshchoudhary@users.noreply.github.com>
2021-02-10 16:01:15 +0530

    sadsa

--- resources/views/f1leaguerules.blade.php
+++ resources/views/f1leaguerules.blade.php
@@ -2,6 +2,6 @@
 
 @section('body')
 <div class="container flex mx-auto h-screen w-full">
-	<iframe width="100%" src="https://docs.google.com/document/d/1Kn8Pmd_AG42X4MgARYpX_jtNb0OnRxyC0iPfAVRLjBo/edit?usp=sharing" title="F1 League Rules"></iframe>
+	<embed src="IRC_Rules__Regs_V1.pdf" class='h-screen w-full' alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">
 </div>
 @endsection
