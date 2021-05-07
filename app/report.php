0c110f499f1318d3b8f92d8f0f1fd915cbc33558
Kapil Bharath <kapilace6@gmail.com>
2021-05-07 12:51:57 +0530

    Fillable Report Table Fields

--- app/report.php
+++ app/report.php
@@ -7,12 +7,15 @@ use Spatie\Activitylog\Traits\LogsActivity;
 class Report extends Model
 {
     use LogsActivity;
-    protected static $logName = 'report';  // Name for the log 
+    protected static $logName = 'report';    // Name for the log
     protected static $logAttributes = ['*']; // Log All fields in the table
-    protected static $logOnlyDirty = true; // Only log the fields that have been updated
+    protected static $logOnlyDirty = true;   // Only log the fields that have been updated
 
     protected $fillable = [
-         'against', 'track','inquali','lap','explained','proof'
+         'reporting_driver', 'reported_against', 'race_id',
+         'lap', 'explanation', 'verdict_message', 'proof',
+         'stewards_notes', 'verdict_pp', 'verdict_time',
+         'resolved'
     ];
 
 }
