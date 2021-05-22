db3fa67acf82aa4190827ed6cf3ee6dd275c21c4
Snehasish Dawn <snehasishdawn29@gmail.com>
2021-05-23 00:22:49 +0530

    fix: tier not updating properly

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -31,7 +31,7 @@ class DriverController extends StandingsController
   {
     return view('admin.edit')->with('user', $user);
   }
- 
+
   public function saveedit(User $user)
   {
     $data = request()->all();
@@ -67,32 +67,33 @@ class DriverController extends StandingsController
   {
     $existing = Driver::where('user_id', $id->id)->get();
     return view('admin.allot')
-            ->with('user', $id)
-            ->with('existing', $existing);
+      ->with('user', $id)
+      ->with('existing', $existing);
   }
 
   public function saveallotment()
   {
     $data = request()->all();
     $userinfo = User::select('*')
-                    ->where('id', $data['user_id'])
-                    ->get()->toArray();
+      ->where('id', $data['user_id'])
+      ->get()->toArray();
 
     $driver = new Driver();
-    $driver -> user_id = $data['user_id'];
-    $driver -> name = $userinfo['0']['name'];
-    $driver -> drivernumber = 5;
-    $driver -> retired = 0;
-    $driver -> alias = $userinfo['0']['name'];
+    $driver->user_id = $data['user_id'];
+    $driver->name = $userinfo['0']['name'];
+    $driver->drivernumber = 5;
+    $driver->retired = 0;
+    $driver->alias = $userinfo['0']['name'];
+    $driver->tier = $data['tier'];
     $driver->save();
 
     return redirect()->back();
   }
-  
+
   public function driverdata()
   {
-    $driver = Driver::select('id','name','tier','team','drivernumber','user_id')
-                      ->get()->load('user:id,name,avatar,steam_id,xbox')->toArray();
+    $driver = Driver::select('id', 'name', 'tier', 'team', 'drivernumber', 'user_id')
+      ->get()->load('user:id,name,avatar,steam_id,xbox')->toArray();
 
     $constructor = Constructor::all()->toArray();
 
@@ -100,25 +101,25 @@ class DriverController extends StandingsController
     $ts = array();
 
     //Iterate through all Active Seasons
-    for($i = 0; $i < count($seasons); ++$i) {
-      for($j = 0; $j < count($driver); ++$j)
+    for ($i = 0; $i < count($seasons); ++$i) {
+      for ($j = 0; $j < count($driver); ++$j)
         $driver[$j][$seasons[$i]['id']] = 0;
-      for($j = 0; $j < count($constructor); ++$j)
+      for ($j = 0; $j < count($constructor); ++$j)
         $constructor[$j][$seasons[$i]['id']] = 0;
 
       //Results for this Season
       $ts = $this->computeStandings($seasons[$i]['series'], $seasons[$i]['tier'], $seasons[$i]['season']);
-      if($ts['code'] != 200)
+      if ($ts['code'] != 200)
         continue;
 
       //Add Points to Drivers
-      for($j = 0; $j < count($ts['drivers']); ++$j) {
+      for ($j = 0; $j < count($ts['drivers']); ++$j) {
         $d_id = array_search($ts['drivers'][$j]['id'], array_column($driver, "id"));
         $driver[$d_id][$seasons[$i]['id']] = $ts['drivers'][$j]['points'];
       }
 
       //Add Points to Constructors
-      for($j = 0; $j < count($ts['constructors']); ++$j) {
+      for ($j = 0; $j < count($ts['constructors']); ++$j) {
         $c_id = array_search($ts['constructors'][$j]['id'], array_column($constructor, "id"));
         $constructor[$c_id][$seasons[$i]['id']] = $ts['constructors'][$j]['points'];
       }
