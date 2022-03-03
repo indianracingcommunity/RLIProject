88f55dacb02c00e70a1c8698e4af392c8b5125d1
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-03-03 20:34:25 +0530

    Slider component and code converted to pure php for Team page

--- resources/views/ourteam.blade.php
+++ resources/views/ourteam.blade.php
@@ -1,93 +1,50 @@
 @extends('layouts.app')
-
 @section('body')
+@component('layouts.slickCarousal')@endcomponent
 
 <div class="mx-4 my-10 md:mx-16 lg:mx-16">
 	<div class="flex font-extrabold text-4xl mb-4">
 		Meet the Team!
 	</div>
-	<div class="grid grid-cols-2 mb-8 md:grid-cols-4 lg:grid-cols-8" id="role_tab"></div>
+	<div class="grid grid-cols-2 mb-8 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7" id="role_tab">
+        @foreach($data as $roleData)
+            <button class="items-center justify-center mr-4 my-4 px-4 flex py-2 bg-purple-600 text-white text-lg rounded-full font-semibold shadow-md cursor-pointer hover:bg-purple-800 hover:text-white hover:shadow-none @if($loop->first) activeBtn @endif buttonAll btn_{{$roleData['role_id']}}" onclick="displayThis('{{$roleData['role_id']}}')">
+                <span class="mx-2">{{$roleData['role_name']}}</span>
+            </button>
+        @endforeach
+    </div>
 
-    <div class="w-full grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3" id="team_container">
-        <div class="flex items-center shadow-xl shadow-gray-800 border-2 border-gray-200 rounded-full">
-            <img
-                src="https://sufyan.me/_nuxt/images/sufyan-e995a0.webp"
-                class="object-cover w-24 h-24 m-4 rounded-full shadow"
-            />
-            <div class="m-4">
-                <p class="text-lg font-bold text-gray-800">Name here</p>
-                <p class="text-sm text-gray-600">CEO &amp; Founder</p>
+    <div class="w-full grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" id="team_container">
+        @foreach($data as $indData)
+            @foreach($indData['users'] as $insideUsers)
+            <div class="role_{{$indData['role_id']}} userPills flex items-center shadow-xl shadow-gray-800 border-2 border-gray-200 rounded-full" style="border-color:#{{$indData['role_color']}}">
+                <img src="{{$insideUsers['avatar']}}" onerror="this.onerror=null;this.src='https://cdn.discordapp.com/embed/avatars/3.png';" class="object-cover w-24 h-24 m-4 rounded-full shadow"/>
+                <div class="m-4">
+                    <p class="text-lg font-bold text-gray-800">{{$insideUsers['name']}}</p>
+                    <p class="text-sm text-gray-600">{{$indData['role_name']}}</p>
+                </div>
             </div>
-        </div>
+            @endforeach
+        @endforeach
     </div>
 </div>
 
 <script type="text/javascript">
     var data = <?php echo json_encode($data); ?>;
-    var index = Array(data.length).fill(0) 
-    console.log(data);
-    displayNewTab(0);
-    function displayNewTab(idx) {
-        var temp_str = '';
-        var unselected = "items-center justify-center mr-4 my-4 px-4 flex py-2 bg-purple-600 text-white text-lg rounded-full font-semibold shadow-md cursor-pointer hover:bg-purple-800 hover:text-white hover:shadow-none ";
-        var selected = "items-center justify-center mr-4 my-4 px-4 flex py-2 bg-gray-800 text-white text-lg rounded-full font-semibold shadow-md cursor-pointer hover:bg-gray-900 hover:text-white hover:shadow-none";
-        index = Array(data.length).fill(0)
-        index[idx] = 1;
-        for(var i=0 ; i < data.length ; i++) {
-            if (index[i] == 0) {
-                if (data[i].icon == null) {
-                    temp_str += `<button class="${unselected}" onclick="displayNewTab(${i})">
-                                    <span class="mx-2"> ${data[i]["role_name"]}</span>
-                                </button>`;
-                }
-                else {
-                    temp_str += `<button class="${unselected}" onclick="displayNewTab(${i})">
-                                    <img
-                                    src="${data[i].icon}"
-                                    class="object-contain w-16 h-8"
-                                    />
-                                    <span class="mx-2"> ${data[i]["role_name"]}</span>
-                                </button>`;
-                }
-            } else {
-                if (data[i].icon == null) {
-                    temp_str += `<button class="${selected}" onclick="displayNewTab(${i})">
-                                    <span class="mx-2"> ${data[i]["role_name"]}</span>
-                                </button>`;
-                }
-                else {
-                    temp_str += `<button class="${selected}" onclick="displayNewTab(${i})">
-                                    <img
-                                    src="${data[i].icon}"
-                                    class="object-contain w-16 h-8"
-                                    />
-                                    <span class="mx-2"> ${data[i]["role_name"]}</span>
-                                </button>`;
-                }
-            }
-        }
-        document.getElementById("role_tab").innerHTML = temp_str;
-        document.getElementById("team_container").scrollIntoView({behavior: "smooth"});
-        fillTeamMembers(idx);
+    // console.log(data);
+    function displayThis(roleId){
+        $('.userPills').hide();
+        $(`.role_${roleId}`).show();
+        $(`.buttonAll`).removeClass('activeBtn');
+        $(`.buttonAll`).removeClass('bg-gray-800 hover:bg-gray-900');
+        $(`.buttonAll`).addClass('bg-purple-600 hover:bg-purple-800');
+        $(`.btn_${roleId}`).removeClass('activeBtn bg-purple-600 hover:bg-purple-800');
+        $(`.btn_${roleId}`).addClass('activeBtn bg-gray-800 hover:bg-gray-900');
     }
-
-    function fillTeamMembers(idx){
-        var temp_str = '';
-        for(var i=0 ; i < data[idx]["users"].length ; i++) {
-            temp_str += `<div class="flex items-center shadow-xl shadow-gray-900 border rounded-full" style="border-color:#${data[idx]["role_color"]};">
-                            <img
-                                src="${data[idx]["users"][i]["avatar"]}"
-                                onerror="this.onerror=null;this.src='https://cdn.discordapp.com/embed/avatars/3.png';"
-                                class="object-cover w-24 h-24 m-4 rounded-full shadow"
-                            />
-                            <div class="m-4">
-                                <p class="text-xl font-bold text-gray-800 md:text-2xl lg:text-2xl">${data[idx]["users"][i]["name"]}</p>
-                                <p class="text-lg text-gray-600">${data[idx]["role_name"]}</p>
-                            </div>
-                        </div>`;
-        }
-        document.getElementById("team_container").innerHTML = temp_str;
-    }
+    $(document).ready(function () {
+        $('.userPills').hide();
+        $('.activeBtn').trigger('click');
+    });
 </script>
 
 @endsection
