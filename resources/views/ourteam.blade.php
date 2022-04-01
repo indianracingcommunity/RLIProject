39c47bbbf1fb56705ffd92b2bd30f3104f6e3167
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-04-02 01:50:48 +0530

    Our team page improvements, acc rules doc update

--- resources/views/ourteam.blade.php
+++ resources/views/ourteam.blade.php
@@ -2,9 +2,14 @@
 @section('body')
 @component('layouts.slickCarousal')@endcomponent
 
+<style>
+    .slick-slider{
+        display: flex !important;
+    }
+</style>
 <div class="mx-4 my-10 md:mx-16 lg:mx-16">
 	<div class="font-extrabold md:text-left text-center text-4xl mb-4">
-		Meet the Team!
+		Meet the Team! <i class='loaderCircle fas fa-circle-notch'></i>
 	</div>
     <div class="font-extrabold text-center text-4xl mb-4 md:hidden">
         <div id="roleSelectDiv">
@@ -18,25 +23,29 @@
             </div>
         </div>
 	</div>
-    
-	<div class="flex items-center hidden md:block" id="role_tab">
+	<div class="flex items-center hidden md:block" style='display:none;' id="role_tab">
         @foreach($data as $roleData)
         <div>
             <button class="items-center justify-center mr-4 my-4 px-4 flex py-2 bg-purple-600 text-white text-lg rounded-full font-semibold shadow-md cursor-pointer hover:bg-purple-800 hover:text-white hover:shadow-none @if($loop->first) activeBtn @endif buttonAll btn_{{$roleData['role_id']}}" onclick="displayThis('{{$roleData['role_id']}}','desk')">
                 <span class="mx-2">{{$roleData['role_name']}}</span>
             </button>
         </div>
-        @endforeach
+        @endforeach 
     </div>
-
-    <div class="w-full grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" style='display:none;' id="team_container">
+    <hr>
+    <div class="pt-4 w-full grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" style='display:none;' id="team_container">
         @foreach($data as $indData)
             @foreach($indData['users'] as $insideUsers)
             <div class="role_{{$indData['role_id']}} userPills flex items-center shadow-xl shadow-gray-800 border-2 border-gray-200 rounded-full" style="border-color:#{{$indData['role_color']}}">
                 <img src="{{$insideUsers['avatar']}}" onerror="this.onerror=null;this.src='https://cdn.discordapp.com/embed/avatars/3.png';" class="object-cover w-24 h-24 m-4 rounded-full shadow"/>
                 <div class="m-4">
                     <p class="text-lg font-bold text-gray-800">{{$insideUsers['name']}}</p>
-                    <p class="text-sm text-gray-600">{{$indData['role_name']}}</p>
+                    @php
+                        $theRole = trim($indData['role_name']);
+                        $roleIs = str_ends_with($theRole, 's') ? substr($theRole,0,-1) : $theRole;
+                        $roleIs = str_contains($theRole,'Team') ? str_replace('Team','Team Member',$theRole) : $roleIs;
+                    @endphp
+                    <p class="text-sm text-gray-600">{{ $roleIs }}</p>
                 </div>
             </div>
             @endforeach
@@ -63,28 +72,36 @@
     }
     $(document).ready(function () {
         $('.userPills').hide();
-        $('#team_container').fadeIn();
         $('.activeBtn').trigger('click');
         var width = $(window).width(); 
         if(width > 600){
             $('#role_tab').slick({
                 dots: false,
-                arrows: false,
+                arrows: true,
                 infinite: false,
-                prevArrow: false,
-                nextArrow: false,
+                prevArrow: "<div class='cursor-pointer px-3'><i class='fas fa-angle-left'></i></div>",
+                nextArrow: "<div class='cursor-pointer px-3'><i class='fas fa-angle-right'></i></div>",
                 speed: 700,
-                slidesToShow: 4,
+                slidesToShow: 6,
                 centerMode: false,
                 variableWidth: true,
-                autoplay: true,
+                // autoplay: true,
+                // autoplaySpeed: 1000,
                 edgeFriction: 2,
                 swipeToSlide: true,
                 pauseOnFocus: true,
-                autoplaySpeed: 1000,
             });
-        }
-                            
+            setTimeout(function () {
+                $('.loaderCircle').fadeOut();
+                $('#role_tab').fadeIn();
+                $('#team_container').fadeIn();
+            }, 1000);
+        }else{
+            setTimeout(function () {
+                $('.loaderCircle').fadeOut();
+                $('#team_container').fadeIn();
+            }, 1000);
+        }               
     });
 </script>
 
