4f194e14f06a93a114b03f7c6dc9cca08ee33ef4
Kapil Bharath <kapilace6@gmail.com>
2023-08-17 00:25:52 +0530

    running CI for push to any branch but master

--- build/prod/deploy.sh
+++ build/prod/deploy.sh
@@ -3,8 +3,7 @@
 set -e
 
 REMOTE_BRANCH="$(git branch --show-current)"
-echo "Remote Branch = $REMOTE_BRANCH"
-echo "Argument = $1"
+IRC_APP="irc-app"
 
 # Check if branch argument is missing
 if [ -z "$1" ]; then
@@ -21,29 +20,29 @@ fi
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(docker compose exec irc-app php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(docker compose exec $IRC_APP php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
 
     # Update codebase
     git fetch origin "$1"
     git pull origin "$1"
 
     # Install dependencies based on lock file
-    docker compose exec irc-app composer install --no-interaction --prefer-dist --optimize-autoloader
+    docker compose exec $IRC_APP composer install --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
-    docker compose exec irc-app php artisan migrate
+    docker compose exec $IRC_APP php artisan migrate
 
     # Clear cache
-    docker compose exec irc-app php artisan optimize
+    docker compose exec $IRC_APP php artisan optimize
 
     # Reload PHP
-    docker restart irc-app
+    docker restart $IRC_APP
 
 # Waiting for service to run
 sleep 3
 
 # If service is not running, notify admins
-if [ "$( docker container inspect -f '{{.State.Running}}' irc-app )" = "false" ]; then
+if [ "$( docker container inspect -f '{{.State.Running}}' $IRC_APP )" = "false" ]; then
     echo "Service is down!"
     # TODO: Notify admins
 
@@ -51,6 +50,6 @@ if [ "$( docker container inspect -f '{{.State.Running}}' irc-app )" = "false" ]
 fi
 
 # Exit maintenance mode
-docker compose exec irc-app php artisan up
+docker compose exec $IRC_APP php artisan up
 
 echo "Application deployed!"
