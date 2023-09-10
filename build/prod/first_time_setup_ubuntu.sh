ec88360b872e39fde9e5f4b553ffba7ba289d66b
Kapil Bharath <kapilace6@gmail.com>
2023-09-10 20:20:03 +0000

    ci: :lock: added SSL encryption to prod docker compose setup
    * Added Laravel IDE helper dev package for better intellisense
    * Added SSL best practices to Nginx config file
    * Added discord notification to admins in case of failed deployment
    * Added certbot for SSL certificate creation & setup renewal cron in first time setup script
    * Created a testing build directory
    
    closes #207

--- build/prod/first_time_setup_ubuntu.sh
+++ build/prod/first_time_setup_ubuntu.sh
@@ -39,13 +39,61 @@ fi
 mkdir -p ../../storage/backups
 mkdir -p ../../storage/signups
 
-# Start the server
-docker compose -f ../../docker-compose.yml up --build -d
+SERVER_NAME='irc-server';
+CERTBOT_DATA_PATH="../../setup/$APP_ENV/certbot"
+WEBSITE_EMAIL=$(awk '/WEBSITE_EMAIL=/' ../../.env | awk '{split($0,a,"="); print a[2]}');
+WEBSITE_DOMAIN=$(awk '/WEBSITE_DOMAIN=/' ../../.env | awk '{split($0,a,"="); print a[2]}');
+
+mkdir -p $CERTBOT_DATA_PATH
+if [ ! -e "$CERTBOT_DATA_PATH/conf/options-ssl-nginx.conf" ] || [ ! -e "$CERTBOT_DATA_PATH/conf/ssl-dhparams.pem" ]; then
+  echo "Downloading recommended TLS parameters ..."
+  mkdir -p "$CERTBOT_DATA_PATH/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$CERTBOT_DATA_PATH/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$CERTBOT_DATA_PATH/conf/ssl-dhparams.pem"
+fi
+
+echo "Creating dummy certificate for $WEBSITE_DOMAIN ..."
+CERTBOT_DOCKER_PATH="/etc/letsencrypt/live/$WEBSITE_DOMAIN"
+mkdir -p "$CERTBOT_DATA_PATH/conf/live/$WEBSITE_DOMAIN"
+docker compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout '$CERTBOT_DOCKER_PATH/privkey.pem' \
+    -out '$CERTBOT_DOCKER_PATH/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
+
+echo "Starting docker containers ..."
+docker compose -f ../../docker-compose.yml up $SERVER_NAME --build -d
+
+echo "Deleting dummy certificate for $WEBSITE_DOMAIN ..."
+docker compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$WEBSITE_DOMAIN && \
+  rm -Rf /etc/letsencrypt/archive/$WEBSITE_DOMAIN && \
+  rm -Rf /etc/letsencrypt/renewal/$WEBSITE_DOMAIN.conf" certbot
+
+docker compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    -d $WEBSITE_DOMAIN \
+    --email $WEBSITE_EMAIL \
+    --rsa-key-size 4096 \
+    --agree-tos --non-interactive \
+    --force-renewal" certbot
+
+echo "Reloading nginx ..."
+docker compose exec $SERVER_NAME nginx -s reload
 
 # Deploy latest changes
 REMOTE_BRANCH="$(git branch --show-current)"
 ./deploy.sh $REMOTE_BRANCH
 
-# Setup scheduler cron
-PRORJECT_DIR=$(awk '/PRORJECT_DIR=/' ../../.env | awk '{split($0,a,"="); print a[2]}');
-crontab -l | { cat; echo "0 * * * * cd $PROJECT_DIR && php artisan schedule:run >> /dev/null 2>&1"; } | crontab -
+# Setup crons
+echo "Setting up crons"
+PROJECT_DIR=$(awk '/PROJECT_DIR=/' ../../.env | awk '{split($0,a,"="); print a[2]}');
+# Run cron: Every hour
+crontab -l | { cat; echo "0 * * * * cd $PROJECT_DIR && \
+    php artisan schedule:run >> /dev/null 2>&1"; } | crontab -
+
+# Run cron: At 04:00, on day 5 of the month
+crontab -l | { cat; echo "0 4 5 * * cd $PROJECT_DIR && \
+    docker compose run --rm --entrypoint \"certbot renew --non-interactive\" certbot && \
+    docker compose exec $SERVER_NAME nginx -s reload >> /dev/null 2>&1"; } | crontab -
