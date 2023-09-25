55d5fbf43b529fee90de4fc0d0b4e1e6eb7b096d
Kapil Bharath <kapilace6@gmail.com>
2023-09-25 06:54:58 +0000

    fix: :card_file_box: reformatted points table migration on renaming columns to identify current state of db
    In the case that columns are manually added to the DB (in the old format), the migration did not previously modify those columns. The migration naively adds a column to the database, creating two instances. One with the data (in the old format), and the copy (in the new format) with all zeroes. This change ensures that irrespective of the state of the database, the end state is in the new format with no data is lost, be it in the up or down migration.

--- database/migrations/2022_03_20_174614_add_post10_columns_to_points_table.php
+++ database/migrations/2022_03_20_174614_add_post10_columns_to_points_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Points;
 
 class AddPost10ColumnsToPointsTable extends Migration
 {
@@ -14,8 +15,13 @@ class AddPost10ColumnsToPointsTable extends Migration
     public function up()
     {
         Schema::table('points', function (Blueprint $table) {
+            $presentColumns = Schema::getColumnListing('points');
             for ($i = 11; $i < 31; ++$i) {
-                $table->integer('P' . $i)->default(0);
+                if (in_array(strval($i), $presentColumns)) {
+                    $table->renameColumn("`" . strval($i) . "`", 'P' . $i);
+                } else {
+                    $table->integer('P' . $i)->default(0);
+                }
             }
         });
     }
@@ -27,9 +33,24 @@ class AddPost10ColumnsToPointsTable extends Migration
      */
     public function down()
     {
+        // Is there non-zero data in the column?
+            // If so, rename the column without the P
+            // Else, drop the column
         Schema::table('points', function (Blueprint $table) {
+            $pointsColumnList = [];
             for ($i = 30; $i > 10; --$i) {
-                $table->dropColumns('P' . $i);
+                array_push($pointsColumnList, DB::raw("SUM(`P" . $i . "`) as `P" . $i . "`"));
+            }
+            $pointsSumPerColumn = Points::select($pointsColumnList)
+                                        ->first()->toArray();
+
+            foreach ($pointsSumPerColumn as $pointsColumn => $pointsSum) {
+                if ($pointsSum == "0") {
+                    $table->dropColumn($pointsColumn);
+                } else {
+                    $newName = "`" . substr($pointsColumn, 1) . "`";
+                    $table->renameColumn($pointsColumn, $newName);
+                }
             }
         });
     }
