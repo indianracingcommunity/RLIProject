2175c062bd22132353ce46af95cbe5a670de5a76
kapilace6 <kapilace6@gmail.com>
2024-02-25 07:25:09 +0000

    feat: :card_file_box: added series, title fields to Constructors table & title to Circuits table

--- database/factories/ConstructorFactory.php
+++ database/factories/ConstructorFactory.php
@@ -2,10 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Series;
 use App\Constructor;
 use Faker\Generator as Faker;
 
-$factory->define(Constructor::class, function (Faker $faker) {
+$factory->define(Constructor::class, function (Faker $faker, $params) {
+    $seriesId = (array_key_exists("series", $params)) ? $params['series'] : factory(Series::class)->create();
+
     $createdAt = $faker->optional()->datetime();
     $updatedAt = $faker->optional()->datetime();
 
@@ -20,6 +23,10 @@ $factory->define(Constructor::class, function (Faker $faker) {
         'car' => $faker->optional()->randomElement([
             'https://www.f1gamesetup.com/img/teams/2021/ferrari-2021.png'
         ]),
+
+        'series' => $seriesId,
+        'title' => $faker->company,
+
         'created_at' => ($createdAt != null) ? $createdAt->format('Y-m-d H:i:s') : $createdAt,
         'updated_at' => ($updatedAt != null) ? $updatedAt->format('Y-m-d H:i:s') : $updatedAt
     ];
